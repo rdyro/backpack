0dc29fec4dedfa7b4631027cd834aa878cfb4487
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2021-05-14 08:17:38 +0200

    [core] Support sub-sampling in first Dropout derivative

--- backpack/core/derivatives/dropout.py
+++ backpack/core/derivatives/dropout.py
@@ -1,13 +1,15 @@
 from torch import eq
 
 from backpack.core.derivatives.elementwise import ElementwiseDerivatives
+from backpack.core.derivatives.subsampling import subsample_output
 
 
 class DropoutDerivatives(ElementwiseDerivatives):
     def hessian_is_zero(self):
         return True
 
-    def df(self, module, g_inp, g_out):
+    def df(self, module, g_inp, g_out, subsampling=None):
         scaling = 1 / (1 - module.p)
-        mask = 1 - eq(module.output, 0.0).float()
+        output = subsample_output(module, subsampling=subsampling)
+        mask = 1 - eq(output, 0.0).float()
         return mask * scaling
